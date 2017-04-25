df410fc24cb007205d818c99aadda9957bb95ff1
robertjosephwayne <robertjwayne@gmail.com>
2017-04-25 18:40:02 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,36 +1,28 @@
 class Show < ActiveRecord::Base
-
-  # Returns the highest value in the ratings column
   def self.highest_rating
     Show.maximum("rating")
   end
 
-  # Returns the show with the highest rating
   def self.most_popular_show
     Show.find_by(rating: self.highest_rating)
   end
 
-  # Returns the lowest value in the ratings column
   def self.lowest_rating
     Show.minimum("rating")
   end
 
-  # Returns the show with the lowest rating
   def self.least_popular_show
     Show.find_by(rating: self.lowest_rating)
   end
 
-  # Returns the sum of all of the ratings
   def self.ratings_sum
     Show.sum("rating")
   end
 
-  # Returns an array of all of the shows that have a rating greater than 5
   def self.popular_shows
     Show.where("rating > 5")
   end
 
-  # Returns an array of all of the shows sorted by alphabetical order according to their names.
   def self.shows_by_alphabetical_order
     Show.order(:name)
   end
