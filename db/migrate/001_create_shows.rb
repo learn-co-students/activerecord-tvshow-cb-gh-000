7005fc685167c929ff776a83390f5357488ef3f8
robertjosephwayne <robertjwayne@gmail.com>
2017-04-25 13:05:19 -0400

    Fix typo in ActiveRecord::Migration, add network and season columns to shows table

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,9 +1,10 @@
-class CreateShows < ActiveRecord::Migrate
-  def create
+class CreateShows < ActiveRecord::Migration
+  def change
     create_table :shows do |t|
       t.string :name
-      t.string :name
+      t.string :network
       t.string :day
+      t.string :season
       t.integer :rating
     end
   end
